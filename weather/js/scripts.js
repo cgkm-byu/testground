ab103d396c0fb9a3e22f12cc1f116e429a63ce8b
kleinloki <kleinloki@byui.edu>
2020-02-05 09:16:09 -0700

    preston page

---
+++ weather/js/scripts.js
@@ -0,0 +1,6 @@
+onload = function(){
+    document.getElementById("lastModified").innerHTML = "Last Updated " + document.lastModified;
+}
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("hide");
+}
